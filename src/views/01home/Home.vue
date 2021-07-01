f221c532e1fd70e44b867f176c509087c26fda6b
WiseSilence <boykvn@gmail.com>
2021-07-01 23:29:59 +0800

    '导航插槽'

--- src/views/01home/Home.vue
+++ src/views/01home/Home.vue
@@ -1,13 +1,28 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "Home"
-}
+  import NavBar from 'components/common/navbar/NavBar'
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    }
+  }
 </script>
 
 <style scoped>
+  #home {
+    /*position: relative;*/
+    height: 100vh;
+  }
 
+  .home-nav {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
 </style>
