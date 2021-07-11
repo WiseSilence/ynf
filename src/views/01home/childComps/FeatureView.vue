26dc7fe4607bb8bed2ec7329fb453311ca79c9ef
WiseSilence <boykvn@gmail.com>
2021-07-11 18:16:00 +0800

    本周流行和吸引导航

--- src/views/01home/childComps/FeatureView.vue
+++ src/views/01home/childComps/FeatureView.vue
@@ -1,43 +1,43 @@
 <template>
   <div class="feature">
-    <div class="feature-item" v-for="(item, index) in features">
-      <a :href="item.link">
-        <img :src="item.image" alt="">
-        <div>{{item.title}}</div>
+<!--    <div class="feature-item" v-for="(item, index) in features">-->
+      <a href="http://127.0.0.1:8000/users.json">
+        <img src="~assets/img/home/recommend_bg.jpg" alt="">
+<!--        <div>{{item.title}}</div>-->
       </a>
-    </div>
+<!--    </div>-->
   </div>
 </template>
 
 <script>
 	export default {
 		name: "FeatureView",
-    props: {
-		  features: {
-		    type: Array,
-        default: []
-      }
-    }
+    // props: {
+		//   features: {
+		//     type: Array,
+    //     default: []
+    //   }
+    // }
 	}
 </script>
 
 <style scoped>
-  .feature {
-    display: flex;
-    margin-top: 10px;
-    font-size: 14px;
-    padding-bottom: 30px;
-    border-bottom: 10px solid #eee;
-  }
+  /*.feature {*/
+  /*  display: flex;*/
+  /*  margin-top: 10px;*/
+  /*  font-size: 14px;*/
+  /*  padding-bottom: 30px;*/
+  /*  border-bottom: 10px solid #eee;*/
+  /*}*/
 
-  .feature-item {
-    flex: 1;
-    text-align: center;
-  }
+  /*.feature-item {*/
+  /*  flex: 1;*/
+  /*  text-align: center;*/
+  /*}*/
 
   .feature img {
-    width: 80px;
-    height: 80px;
+    width: 100%;
+    /*height: 80px;*/
     margin-bottom: 10px;
   }
 </style>
